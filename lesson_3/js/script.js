1c17b5054bb1c1e03cde5be90de1327480f7d3f9
Andrii Romanenko <125657585+brilliantic@users.noreply.github.com>
2023-09-16 14:36:51 +0300

    Finalized the project
    Changed the tooltip and added pictures on hover using JS

--- lesson_3/js/script.js
+++ lesson_3/js/script.js
@@ -1,9 +1,28 @@
-// Отримати кнопку "Навчання" і випадаючий список
-const dropdownTrigger = document.getElementById("dropdown-trigger");
-const dropdownContent = dropdownTrigger.querySelector(".dropdown-content");
+const link = document.getElementById("myLink");
 
-// Додати обробник подій для кнопки "Навчання"
-dropdownTrigger.addEventListener("click", function () {
-    dropdownContent.style.display =
-        dropdownContent.style.display === "block" ? "none" : "block";
+// Додаємо подію для відображення tooltip при наведенні на посилання
+link.addEventListener("mouseover", () => {
+    const tooltip = document.createElement("div");
+    tooltip.classList.add("tooltip");
+
+    const image = document.createElement("img");
+    image.src = "your-image.jpg";
+    image.alt = "Зображення";
+
+    const text = document.createTextNode("Текст у tooltip");
+
+    tooltip.appendChild(image);
+    tooltip.appendChild(text);
+
+    link.appendChild(tooltip);
+    
 });
+
+// Видаляємо tooltip при знятті курсора
+link.addEventListener("mouseout", () => {
+    const tooltip = link.querySelector(".tooltip");
+    if (tooltip) {
+        link.removeChild(tooltip);
+    }
+});
+
