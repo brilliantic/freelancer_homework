e2f9a05eb4ad8bcfb41fd72a2e2e608680ff71b1
brilliantic <andrii.romanenko.js@gmail.com>
2023-12-10 21:54:45 +0200

    added a clean draft of lesson 17

--- lesson_17/js/formatDate.js
+++
@@ -1,9 +0,0 @@
-function formatDate(input) {
-    let value = input.value.replace(/\D/g, "");
-
-    if (value.length > 2) {
-        value = value.substring(0, 2) + "/" + value.substring(2);
-    }
-
-    input.value = value;
-}
